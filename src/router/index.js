a2b14f447f6f11993804460f118765e2657f7b9e
GuanSzzz <GuanSzzz>
2024-03-11 22:34:14 +0800

    注释route

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,46 @@
-// import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 // vue-router简单版
-import {
-  createRouter,
-  createWebHashHistory,
-} from './grouter/index'
-const router = createRouter({
-history: createWebHashHistory(),
-// routes??
-})
+// import { createRouter, createWebHashHistory } from './grouter/index'
 import Home from '@/views/Home.vue'
 import About from '@/views/About.vue'
 // const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
+//   history: createWebHashHistory(),
 //   routes: [
-//     {
-//       path: '/',
-//       name: 'Home',
-//       component: Home
-//     },
-//     {
-//       path: '/about',
-//       name: 'About',
-//       component: About
-//     }
-//     // {
-//     //   path: '/about',
-//     //   name: 'about',
-//     //   // route level code-splitting
-//     //   // this generates a separate chunk (About.[hash].js) for this route
-//     //   // which is lazy-loaded when the route is visited.
-//     //   component: () => import('../views/AboutView.vue')
-//     // }
-//   ]
+//         {
+//           path: '/',
+//           name: 'Home',
+//           component: Home
+//         },
+//         {
+//           path: '/about',
+//           name: 'About',
+//           component: About
+//         }
+//       ]
 // })
 
-
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    }
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../views/AboutView.vue')
+    // }
+  ]
+})
 
 export default router
